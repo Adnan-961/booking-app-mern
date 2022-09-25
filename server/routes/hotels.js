cd9c54b7d7c24c4b650cde3ff82f95b1dc9e32a0
Adnan-961 <adnan.sayed.official@gmail.com>
2022-09-26 00:59:11 +0300

    fininshed small section

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -1,34 +1,19 @@
 import express from "express";
-import { createHotel, deleteHotel, updateHotel } from "../controllers/hotel.js";
+import {
+  createHotel,
+  deleteHotel,
+  getAllHotels,
+  getHotel,
+  updateHotel,
+} from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js";
+
 const router = express.Router();
 
 router.post("/", createHotel);
-
-router.put("/:id",updateHotel);
-
+router.put("/:id", updateHotel);
 router.delete("/:id", deleteHotel);
-  
-  router.get("/:id", async (req, res) => {
-      try {
-      const getHotel =await Hotel.findById(req.params.id)
-
-        res.status(200).json(getHotel)
-      } catch (error) {
-        res.status(500).json(error);
-      }
-    });
-
-    
-    router.get("/", async (req, res) => {
-        try {
-          const hotels =await Hotel.find();
-          res.status(200).json(hotels)
-            } catch (error) {
-          res.status(500).json(error);
-        }
-      });
-      
-
+router.get("/:id", getHotel);
+router.get("/", getAllHotels);
 
 export default router;
